5201a0cc76ef928c621cdcfe800bf25e57d04495
Neal Bryan Velasco <bonchyvelasco@outlook.com>
2017-05-18 17:39:39 +0800

    Added semantic-ui, jquery, and a welcome page with log-in form

---
+++ application/views/pages/home.php
@@ -0,0 +1,17 @@
+<div class = "ui raised segment" style = "margin-top: 20px; max-width:400px;">
+    <h2 class="ui header">Log-In</h2>
+
+    <div class="ui form" style = "margin-bottom:10px;">
+        <div class="required field">
+            <label>Username</label>
+            <input type="text" autofocus placeholder="Username">
+        </div>
+
+        <div class="required field">
+            <label>Password</label>
+            <input type="password" placeholder="Password">
+        </div>
+
+        <div class = "ui submit button">Submit</div>
+    </div>
+</div>
