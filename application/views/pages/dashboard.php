bb8d9c611b5d350ed404a711a1437f62396f964c
Neal Bryan Velasco <bonchyvelasco@outlook.com>
2017-05-22 17:11:47 +0800

    Added admin functions forms

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -9,7 +9,9 @@
         "birthday" => '1998-09-14',
         "username" => "bonchykels",
         "name" => "Bonchy",
-        "address" => "MNL"
+        "address" => "MNL",
+        "is_admin" => true,
+        "is_online" => true
     );
 
     $users = array(
@@ -17,91 +19,43 @@
             "birthday" => '1998-09-14',
             "username" => "jenny",
             "name" => "JENIFEERR",
-            "address" => "MNL"
+            "address" => "MNL",
+            "is_admin" => false,
+            "is_online" => true
         ),
         array(
             "birthday" => '1998-09-14',
             "username" => "bonchykels",
             "name" => "Person2",
-            "address" => "MNL"
+            "address" => "MNL",
+            "is_admin" => false,
+            "is_online" => false
         ),
         array(
             "birthday" => '1998-09-14',
             "username" => "bonchykels",
             "name" => "Person3",
-            "address" => "QC"
-        ),
-        array(
-            "birthday" => '1998-09-14',
-            "username" => "bonchykels",
-            "name" => "Person3",
-            "address" => "QC"
-        ),
-        array(
-            "birthday" => '1998-09-14',
-            "username" => "bonchykels",
-            "name" => "Person3",
-            "address" => "QC"
-        ),
-        array(
-            "birthday" => '1998-09-14',
-            "username" => "bonchykels",
-            "name" => "Person3",
-            "address" => "QC"
-        ),
-        array(
-            "birthday" => '1998-09-14',
-            "username" => "bonchykels",
-            "name" => "Person3",
-            "address" => "QC"
-        ),
-        array(
-            "birthday" => '1998-09-14',
-            "username" => "bonchykels",
-            "name" => "Person3",
-            "address" => "QC"
-        ),
-        array(
-            "birthday" => '1998-09-14',
-            "username" => "bonchykels",
-            "name" => "Person3",
-            "address" => "QC"
-        ),
-        array(
-            "birthday" => '1998-09-14',
-            "username" => "bonchykels",
-            "name" => "Person3",
-            "address" => "QC"
-        ),
-        array(
-            "birthday" => '1998-09-14',
-            "username" => "bonchykels",
-            "name" => "Person3",
-            "address" => "QC"
-        ),
-        array(
-            "birthday" => '1998-09-14',
-            "username" => "bonchykels",
-            "name" => "Person3",
-            "address" => "QC"
-        ),
+            "address" => "QC",
+            "is_admin" => true,
+            "is_online" => true
+        )
     );
 ?></h1>
 
-<div id = "header" class = "ui basic segment" style = "margin-top: 20px">
-    <h1 class="ui header">
-        <i class="user icon"></i>
-        <div class = "content">
-            <?php echo $luser['name']; ?>
-            <a id = "edit" style = "cursor:pointer"><sup><i class="edit icon"></i></sup></a>
-            <div class="sub header">@<?php echo $luser['username']; ?></div>
-        </div>
-    </h1>
-    <p><?php echo $luser['address']; ?></p>
-    <p><?php echo $luser['birthday']; ?></p>
-</div>
-
-<div id = "form" class = "ui basic segment" style = "margin-top: 20px">
+<div class = "ui basic segment" style = "margin-top: 20px">
+    <div class = "reg">
+        <h1 class="ui header">
+            <i class="user icon"></i>
+            <div class = "content">
+                <?php echo $luser['name']; ?>
+                <sup><i class="blue edit link icon"></i></sup>
+                <div class="sub header">@<?php echo $luser['username']; ?></div>
+            </div>
+        </h1>
+        <p><?php echo $luser['address']; ?></p>
+        <p><?php echo $luser['birthday']; ?></p>
+    </div>
+    
     <div class="ui form">
         <div class="ui transparent input">
             <h1 class = "ui header">
@@ -113,25 +67,39 @@
             </h1>
         </div>
         <br>
-        <div class="ui input" style = "margin: 5px 0 2.5px 0;">
+        <div class="ui small input" style = "margin: 5px 0 2.5px 0;">
             <input type="text" placeholder="Name" value = "<?php echo $luser['address']; ?>">
         </div>
         <br>
-        <div class="ui input" style = "margin: 2.5px 0 5px 0;">
+        <div class="ui small input" style = "margin: 2.5px 0 5px 0;">
             <input type="date" placeholder="Name" value = "<?php echo $luser['birthday']; ?>">
         </div>
         <br>
-        <div class = "ui submit button">Submit</div>
+        <div class = "ui primary submit labeled icon button">
+            <i class = "save icon"></i>
+            Save
+        </div>
+        <div class = "ui red labeled icon cancel button">
+            <i class = "remove icon"></i>
+            Cancel
+        </div>
     </div>
 </div>
 
+
 <script>
     $(document).ready(function(){
-        $("#form").hide();
-        $("#edit").click(function(){
-            $("#header").hide();
-            $("#form").show();
-            $("#name").focus();
+        $(".form").hide();
+        $(".edit").click(function(){
+            $(this).closest(".list").children().find(".frm").hide();
+            $(this).closest(".list").children().find(".reg").show();
+            $(this).closest(".basic, .item").find(".form, .frm").show();
+            $(this).closest(".basic, .item").find(".form").find("input:first").focus();
+            $(this).closest(".basic, .item").find(".reg").hide();
+        });
+        $(".cancel").click(function() {
+            $(this).closest(".basic, .item").find(".form, .frm").hide();
+            $(this).closest(".basic, .item").find(".reg").show();
         });
         $(".item").hover(function(){
             $(this).css('background', '#f7f7f7');
@@ -145,16 +113,19 @@
     Online Users
 </div>
 
-<div class="ui relaxed list" style="-webkit-column-count: 3; -moz-column-count: 3; column-count: 3;">
-    <?php foreach($users as $user) {?>
-        <div class="item" style = "cursor:pointer; padding: 5px;">
-            <i class="user large icon"></i>
-            <div class="content">
-                <div class = "sub header"><?php echo $user['name']; ?></div>
-                <span style = "color:grey">@<?php echo $user['username']; ?></span>
+<div class="ui relaxed list">
+    <?php foreach($users as $user) {
+        if ($user['is_online'] == true) {?>
+            <div class="item" style = "cursor:pointer; padding: 5px;">
+                <i class="user large icon"></i>
+                <div class="content">
+                    <div class = "sub header"><?php echo $user['name']; ?></div>
+                    @<?php echo $user['username']; ?>
+                </div>
             </div>
-        </div>
-    <?php } ?>
+    <?php 
+        }
+    } ?>
 </div>
 
 <div class="ui horizontal divider">
@@ -164,13 +135,48 @@
 <div class="ui middle aligned relaxed list">
     <?php foreach($users as $user) {?>
         <div class="item" style = "cursor:pointer; padding:10px">
-            <div class="left floated content">
-                <i class="user large icon"></i>
-                <?php echo $user['name']; ?> <span style = "color:grey">@<?php echo $user['username']; ?></span>
+            <div class = "reg">
+                <div class="left floated content">
+                    <i class="user large icon"></i>
+                    <?php echo $user['name']; ?> <span style = "color:grey">@<?php echo $user['username']; ?></span>
+                    <tab>
+                    <?php if($luser['is_admin']) {
+                        echo $user['address'];
+                    } ?>
+                </div>
+                <div class="right floated content">
+                    <?php echo $user['birthday']; ?>
+                    <?php if($luser['is_admin']) { ?>
+                        <i class="edit link icon"></i>
+                    <?php } ?>
+                </div>
             </div>
-            <div class="right floated content">
-                <?php echo $user['birthday']; ?>
+            <?php if($luser['is_admin']) { ?>
+            <div class = "frm">
+                <div class = "left floated content">
+                    <div class="ui form">
+                        @<?php echo $user['username'];?>
+                        <div class="ui input" style = "margin-right:-5px;margin-left:5px;">
+                            <input type="text" placeholder="Name" value = "<?php echo $user['name']; ?>">
+                        </div>
+                        <div class="ui input" style = "padding: 15px;">
+                            <input type="text" placeholder="Address" value = "<?php echo $user['address']; ?>">
+                        </div>
+                        <div class="ui small input" style = "margin: 2.5px 0 5px 0;">
+                            <input type="date" placeholder="yyyy-mm-dd" value = "<?php echo $user['birthday']; ?>">
+                        </div>
+                        <div class = "ui primary submit labeled icon button">
+                            <i class = "save icon"></i>
+                            Save
+                        </div>
+                        <div class = "ui red labeled icon cancel button">
+                            <i class = "remove icon"></i>
+                            Cancel
+                        </div>
+                    </div>
+                </div>
             </div>
+            <?php } ?>
         </div>
     <?php } ?>
 </div>
